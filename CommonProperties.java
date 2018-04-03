631fb08f5f62011a94133976d5525ca3f67fb3d2
yarafath8 <37270534+yarafath8@users.noreply.github.com>
2018-04-03 15:13:10 +0530

    Update CommonProperties.java

--- CommonProperties.java
+++ CommonProperties.java
@@ -20,22 +20,23 @@ public class ObjectProperties {
 	protected static XSSFWorkbook workbook1;
 	protected static XSSFCell Cell;
     
+	//To Open & load Excel Sheet
 	public static void setExcelFile(String SheetName) throws Exception {
         FileInputStream src = new FileInputStream(filepath);
         workbook1 = new XSSFWorkbook(src);
         sheet = workbook1.getSheet(SheetName);
        }
 	
+	//To Launch Windows Desktop Application
 	public void OpenWindowsApplication() throws IOException, Exception {
-	 
 	 DesktopOptions options= new DesktopOptions();
-     options.setApplicationPath(EXEPath);
-     WiniumDriverService service = new WiniumDriverService.Builder().usingPort(9999).withVerbose(true).withSilent(false).buildDesktopService();
+   	 options.setApplicationPath(EXEPath);
+     	 WiniumDriverService service = new WiniumDriverService.Builder().usingPort(9999).withVerbose(true).withSilent(false).buildDesktopService();
 	 WPF1=new WiniumDriver(service,options);
-     
- }
+ 	 }
 
-	File src=new File("D:\\New Workspace\\Scheduling\\ObjectRepo");	  
+	//To load object from Object Properties file (Object Repository)
+	File src=new File(PropertiesFilePath);	  
 	FileInputStream fis;
 	protected static Properties pro=new Properties();
 	{
@@ -53,4 +54,20 @@ public class ObjectProperties {
 	System.out.println("Property class loaded");
 	
 	}	
+	
+	public void login() throws Exception
+	{
+
+		//For load a particular sheet from Excel and use data
+		CommonProperties.setExcelFile("Login");
+		for(int i=1; i<=sheet.getLastRowNum(); i++)
+		{ 
+		 XSSFCell cell = sheet.getRow(i).getCell(0);
+		 A1.findElement(By.xpath(pro.getProperty("Username"))).sendKeys(cell.getStringCellValue());
+		 cell = sheet.getRow(i).getCell(1);         
+		 A1.findElement(By.xpath(pro.getProperty("Password"))).sendKeys(cell.getStringCellValue()); 
+		 A1.findElement(By.xpath(pro.getProperty("LoginBtn"))).click();
+		}
+		
+	}
 }
